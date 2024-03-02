f8cdc5292951c71f618e9549b6d87dcfdfc0df3a
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-03-03 02:58:09 +0530

    Your commit message here

---
+++ backend/models/user.model.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    gender: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    dob: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    parentEmail: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  //createdAt, updatedAt => Member Since <createdAt> <date>
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ backend/server.js
@@ -0,0 +1,28 @@
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express from "express";
+
+import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
+import connectToMongoDB from "../db/connnectToMongoDB.js";
+import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+app.use(express.json()); //to parse incoming request from jason payload (from req.body)
+app.use(cookieParser()); // to get cookies from req.cookie.jwt, used for authentication
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server Running on port ${PORT}`);
+});
