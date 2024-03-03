d1763970e30be74fb9354d0dffea8878ac4cd13b
Arijit Char <115190087+Arijit-Char@users.noreply.github.com>
2024-03-03 13:07:34 +0530

    next commit

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,14 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
 app.use(express.json()); //to parse incoming request from jason payload (from req.body)
 app.use(cookieParser()); // to get cookies from req.cookie.jwt, used for authentication
 
